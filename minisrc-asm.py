bb10988cf6c7feb6f36a77c2f780f0ddb701809e
Nikola Zupancic <nikola.z37@hotmail.com>
2024-03-04 10:55:58 -0500

    simplifies some things

--- minisrc-asm.py
+++ minisrc-asm.py
@@ -1,45 +1,31 @@
 import argparse
 
-instr_to_op = {
-    "ld":   0b00000,
-    "ldi":  0b00001,
-    "st":   0b00010,
-    "add":  0b00011,
-    "sub":  0b00100,
-    "shr":  0b00101,
-    "shra": 0b00110,
-    "shl":  0b00111,
-    "ror":  0b01000,
-    "rol":  0b01001,
-    "and":  0b01010,
-    "or":   0b01011,
-    "addi": 0b01100,
-    "andi": 0b01101,
-    "ori":  0b01110,
-    "mul":  0b01111,
-    "div":  0b10000,
-    "neg":  0b10001,
-    "not":  0b10010,
-    "brzr": 0b10011,
-    "brnz": 0b10011,
-    "brmi": 0b10011,
-    "brpl": 0b10011,
-    "jr":   0b10100,
-    "jal":  0b10101,
-    "in":   0b10110,
-    "out":  0b10111,
-    "mfhi": 0b11000,
-    "mflo": 0b11001,
-    "nop":  0b11010,
-    "halt": 0b11011,
-}
+INSTRUCTIONS = [
+    "ld", "ldi", "st", "add", "sub", "shr", "shra", "shl",
+    "ror", "rol", "and", "or", "addi", "andi", "ori",
+    "mul", "div", "neg", "not", "brzr", "brnz", "brmi", "brpl",
+    "jr", "jal", "in", "out", "mfhi", "mflo", "nop", "halt"
+]
 
-branch_to_cond = {
+INSTR_TO_OP = {}
+for i in range(0, len(INSTRUCTIONS)):
+    if i >= 0b10011 and i < 0b10011 + 4:
+        INSTR_TO_OP[INSTRUCTIONS[i]] = 0b10011
+    elif i >= 0b10011 + 4:
+        INSTR_TO_OP[INSTRUCTIONS[i]] = i - 3
+    else:
+        INSTR_TO_OP[INSTRUCTIONS[i]] = i
+
+BRANCH_TO_COND = {
     "brzr": 0b0000,
     "brnz": 0b0001,
     "brmi": 0b0010,
     "brpl": 0b0011,
 }
+
+REG_MAP = {}
+for i in range(16):
+    REG_MAP["r" + str(i)] = i
 
 
 def convert_to_bits(instruction_text):
@@ -50,67 +36,47 @@ def convert_to_bits(instruction_text):
 
     opcode, ra, rb, rc, c2, c_imm = 0, 0, 0, 0, 0, 0
 
-    reg_map = {}
-    for i in range(16):
-        reg_map["r" + str(i)] = i
-
-    opcode = instr_to_op[instruction[0]]
+    opcode = INSTR_TO_OP[instruction[0]]
     if len(instruction) >= 2:
         if instruction[0] == "st":
-            ra = reg_map[instruction[2]]
+            ra = REG_MAP[instruction[2]]
         else:
-            ra = reg_map[instruction[1]]
+            ra = REG_MAP[instruction[1]]
 
     # need revision for signed
     match instruction[0]:
         case "ld" | "ldi":
             parsed = parse_base(instruction[2])
-            rb = reg_map[parsed[1]]
+            rb = REG_MAP[parsed[1]]
             c_imm = parsed[0]
         case "st":
             parsed = parse_base(instruction[1])
-            rb = reg_map[parsed[1]]
+            rb = REG_MAP[parsed[1]]
             c_imm = parsed[0]
         case "add" | "sub" | "shr" | "shra" | "shl" | "ror" | "rol" | "and" | "or":
-            ra = reg_map[instruction[1]]
-            rb = reg_map[instruction[2]]
-            rc = reg_map[instruction[3]]
+            ra = REG_MAP[instruction[1]]
+            rb = REG_MAP[instruction[2]]
+            rc = REG_MAP[instruction[3]]
         case "addi" | "andi" | "ori":
-            ra = reg_map[instruction[1]]
-            rb = reg_map[instruction[2]]
+            ra = REG_MAP[instruction[1]]
+            rb = REG_MAP[instruction[2]]
             c_imm = pint(instruction[3])
         case "mul" | "div" | "neg" | "not":
-            ra = reg_map[instruction[1]]
-            rb = reg_map[instruction[2]]
+            ra = REG_MAP[instruction[1]]
+            rb = REG_MAP[instruction[2]]
         case "brzr" | "brnz" | "brmi" | "brpl":
-            ra = reg_map[instruction[1]]
-            c2 = branch_to_cond[instruction[0]]
+            ra = REG_MAP[instruction[1]]
+            c2 = BRANCH_TO_COND[instruction[0]]
             c_imm = pint(instruction[2])
         case "jr" | "jal" | "in" | "out" | "mfhi" | "mflo":
-            ra = reg_map[instruction[1]]
+            ra = REG_MAP[instruction[1]]
         case "nop" | "halt":
             pass
         case _:
             raise ValueError("invalid instruction")
 
-    instr_numerical = 0
-    match instruction[0]:
-        case "ld" | "ldi" | "st":
-            instr_numerical = (opcode << 27) + (ra << 23) + (rb << 19) + c_imm
-        case "add" | "sub" | "shr" | "shra" | "shl" | "ror" | "rol" | "and" | "or":
-            instr_numerical = (opcode << 27) + (ra << 23) + (rb << 19) + (rc << 15)
-        case "addi" | "andi" | "ori":
-            instr_numerical = (opcode << 27) + (ra << 23) + (rb << 19) + (c_imm & 0x7ffff)
-        case "mul" | "div" | "neg" | "not":
-            instr_numerical = (opcode << 27) + (ra << 23) + (rb << 19)
-        case "brzr" | "brnz" | "brmi" | "brpl":
-            instr_numerical = (opcode << 27) + (ra << 23) + (c2 << 19) + c_imm
-        case "jr" | "jal" | "in" | "out" | "mfhi" | "mflo":
-            instr_numerical = (opcode << 27) + (ra << 23)
-        case "nop" | "halt":
-            instr_numerical = (opcode << 27)
-        case _:
-            instr_numerical = 0
+    instr_numerical = (opcode << 27) + (ra << 23) + (rb << 19) + (rc << 15)
+    + (c2 << 19) + (c_imm & 0x7ffff)
 
     return instr_numerical
 
