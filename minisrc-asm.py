193ea7f8c7e13aebec726fefa5f0f120a77dd1c2
Nikola Zupancic <nikola.z37@hotmail.com>
2024-03-16 03:18:15 -0400

    added tags and directives

--- minisrc-asm.py
+++ minisrc-asm.py
@@ -1,5 +1,9 @@
 import argparse
 
+DIRECTIVES = [
+    "ORG", ".org", "DB", ".db"
+]
+
 INSTRUCTIONS = [
     "ld", "ldi", "st", "add", "sub", "shr", "shra", "shl",
     "ror", "rol", "and", "or", "addi", "andi", "ori",
@@ -28,7 +32,7 @@ for i in range(16):
     REG_MAP["r" + str(i)] = i
 
 
-def convert_to_bits(instruction_text):
+def convert_to_bits(instruction_text, line_num=0, tags=None):
     instruction = instruction_text.lower().replace(",", "").split()
 
     if len(instruction) == 0:
@@ -40,7 +44,7 @@ def convert_to_bits(instruction_text):
     if len(instruction) >= 2:
         if instruction[0] == "st":
             ra = REG_MAP[instruction[2]]
-        else:
+        elif instruction[0] != "halt" and instruction[0] != "stop":
             ra = REG_MAP[instruction[1]]
 
     # need revision for signed
@@ -67,7 +71,11 @@ def convert_to_bits(instruction_text):
         case "brzr" | "brnz" | "brmi" | "brpl":
             ra = REG_MAP[instruction[1]]
             c2 = BRANCH_TO_COND[instruction[0]]
-            c_imm = pint(instruction[2])
+            res = parse_tag(instruction[2], tags)
+            if res is not None:
+                c_imm = res
+            else:
+                c_imm = pint(instruction[2])
         case "jr" | "jal" | "in" | "out" | "mfhi" | "mflo":
             ra = REG_MAP[instruction[1]]
         case "nop" | "halt":
@@ -97,6 +105,13 @@ def convert_to_bits(instruction_text):
     return instr_numerical
 
 
+def parse_tag(val, tags):
+    for tag in tags:
+        if val == tag[0]:
+            return tag[1]
+    return None
+
+
 def setup():
     parser = argparse.ArgumentParser("instrmaker")
     parser.add_argument('-s', "--file_in", type=str, help="The input file to convert into a binary file")
@@ -114,12 +129,65 @@ def convert_text_file(file_in, file_out):
     if out:
         fout = open(file_out, "w")
 
-    for line in fin.readlines():
-        converted = convert_to_bits(line)
-        print('{:<20} : '.format(line[:len(line)]), f"{converted:#0{10}x}")
+    lines = fin.readlines()
+    out_lines = []
+    tags = []
+    line_num = 0
+    orgs = []
+
+    # remove comments
+    i = 0
+    while i < len(lines):
+        if lines[i] == "\n":
+            lines.pop(i)
+            continue
+        else:
+            lines[i] = lines[i].split(";", 1)[0]
+        i = i + 1
+
+    # find all tags
+    # tags represent the address of the instruction directly under them
+
+    for line in lines:
+        # remove comments
+        for directive in DIRECTIVES:
+            if directive in line:
+                toks = line.split()
+                if toks[0] == "ORG":
+                    orgs.append((line_num, pint(toks[1])))
+
+        if ':' in line:
+            if line.replace("\n", "")[-1] == ':':
+                tags.append((line[:len(line)-2], line_num))
+            else:
+                raise ValueError("Invalid Syntax at line: ", line_num, "near: ", line)
+        else:  # only increase line number on lines with actual instructions
+            line_num = line_num + 1
+
+    # have to pass in the tags to know the offset from the line number for jump instructions
+    # branch instructions can have their tags replaced directly
+    # convert instructions
+    line_num = 0
+    # print(repr(tags))
+    # print(repr(orgs))
+    found_org = False
+    for line in lines:
+        if ':' in line:
+            continue
+        for org in orgs:
+            if line_num == org[0]:
+                found_org = True
+                line_num = org[1] + 1
+                break
+        if found_org:
+            found_org = False
+            continue
+        converted = convert_to_bits(line, line_num, tags)
+        out_lines.append(f"{converted:#0{10}x}"[2:])
+        print(line_num, ': {:<30} : '.format(line[:len(line)-1]), f"{converted:#0{10}x}")
         if out:
             fout.write(f"{converted:#0{10}x}"[2:] + "\n")
-
+        line_num = line_num + 1
     if out:
         fout.close()
     fin.close()
